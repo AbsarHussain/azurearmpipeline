d5095cfd1f559ff1aab98f06f7d1f5af229894a2
Absar Hussain <absarhussain12119@gmail.com>
2024-03-04 06:19:08 +0500

    docker ignore

--- aspnetapi/obj/Debug/net8.0/aspnetapi.AssemblyInfo.cs
+++ aspnetapi/obj/Debug/net8.0/aspnetapi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01212212a3fa7a3c51047270d17a3bab8438c071")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
